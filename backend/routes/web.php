a1fd041143f74592c74db9ba1eccfe35766eb8d1
samskritisolutions-dev <dev@samskritisolutions.com>
2026-05-13 16:19:55 +0530

    web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/init-db', function() {
+    Artisan::call('migrate --force');
+    return "Database tables created!";
 });
