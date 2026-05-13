7712c0361ef7964dfe05f178014e836120a42442
Samskriti Solutions Dev <dev@samskritisolutions.com>
2026-05-13 14:22:37 +0530

    Initial commit for deployment

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'https://*.vercel.app', // Allows all your vercel preview/prod deployments
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +33,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
